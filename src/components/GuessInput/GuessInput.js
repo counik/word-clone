27d34c20c66338d5bb5411a01a4dbaa5e766de34
Kevin Counihan <kevin.counihan17@gmail.com>
2023-03-28 12:27:24 -0700

    Disable input instead of removing from DOM when game is over

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -14,21 +14,20 @@ function GuessInput({ saveGuess, gameStatus }) {
       }}
     >
       <label htmlFor="guess-input">Enter guess:</label>
-      {gameStatus === GAME_STATUSES.pending && (
-        <input
-          id="guess-input"
-          type="text"
-          value={guess}
-          maxLength={5}
-          pattern={'^[A-Z]{5}$'}
-          autoComplete="off"
-          onChange={(event) => {
-            const newGuess = event.target.value;
-            const nonStrings = /[^a-zA-z]/;
-            setGuess(newGuess.replace(nonStrings, '').toUpperCase());
-          }}
-        />
-      )}
+      <input
+        id="guess-input"
+        type="text"
+        disabled={gameStatus !== GAME_STATUSES.pending}
+        value={guess}
+        maxLength={5}
+        pattern={'^[A-Z]{5}$'}
+        autoComplete="off"
+        onChange={(event) => {
+          const newGuess = event.target.value;
+          const nonStrings = /[^a-zA-z]/;
+          setGuess(newGuess.replace(nonStrings, '').toUpperCase());
+        }}
+      />
     </form>
   );
 }
